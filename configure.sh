c5b38396030436cc4e8c766245ce1a8a30c40eed
chineseholmes <chineseholmes@gmail.com>
2020-11-30 02:00:11 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,18 +4,29 @@
 mkdir /tmp/frp
 curl -L -H "Cache-Control: no-cache" -o /tmp/frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.34.3_linux_amd64.tar.gz
 tar -xzvf /tmp/frp/frp.tar.gz -C /tmp/frp
-install -m 755 /tmp/frp/frp_0.34.3_linux_amd64/frps /usr/local/bin/frps
+install -m 755 /tmp/frp/frp_0.34.3_linux_amd64/frpc /usr/local/bin/frpc
 
 # Remove temporary directory
 rm -rf /tmp/frp
 
 # Frp new configuration
 install -d /usr/local/etc/frp
-cat << EOF > /usr/local/etc/frp/frps.ini
+cat << EOF > /usr/local/etc/frp/frpc.ini
 [common]
-bind_port = $FRPPORT
-vhost_http_port = 443
+protocol = tcp
+server_addr = us-la-cn2.sakurafrp.com
+server_port = 7000
+user = 9o2ntwefismbyr83
+token = SakuraFrpClientToken
+sakura_mode = true
+use_recover = true
+tcp_mux = true
+pool_count = 1
+[herokuv2]
+type = tcp
+remote_port = 35907
+plugin = http_proxy
 EOF
 
 # Run Frp
-/usr/local/bin/frps -c /usr/local/etc/frp/frps.ini
+/usr/local/bin/frpc -c /usr/local/etc/frp/frpc.ini
